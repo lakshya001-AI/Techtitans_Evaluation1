3a946db882e1bbd1289bda857e0786754c2790d7
Lakshya Makode <makodelakshya101@gmail.com>
2024-09-16 03:03:37 +0530

    commit changes

--- Backend/MongoDB/connect.js
+++ Backend/MongoDB/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connectMongoose() {
   try {
     await mongoose
-      .connect("mongodb://localhost:27017/udyogSetuDatabase")
+      .connect("mongodb+srv://makodelakshya101:BOypb5Jhs4zVemna@cluster0.ujdfe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
       .then((res) => console.log(`MongoDB Database is Connected`))
       .catch((err) => console.log(`Error connecting to Database:` + err));
   } catch (error) {
